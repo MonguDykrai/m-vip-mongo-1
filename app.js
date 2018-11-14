fc8316ae70c989cdafa5ed2a48c0b21529fa0dfa
M <M@qq.com>
2018-11-14 15:03:18 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+
+// Database Name
+const dbName = 'vip';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  console.log(db)
+
+  client.close();
+});
